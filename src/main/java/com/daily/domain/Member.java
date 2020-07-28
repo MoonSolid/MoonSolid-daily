5c434d6776397338a43d2f96f76a0d8595b026ba
MoonSolid <rnreo20@gmail.com>
2020-07-28 17:27:20 +0900

    [mod] Member DTO 프로퍼티 추가
    type,email,tel 프로퍼티 추가
    
    - 수정
     - src/main/java/com/daily/domain/Member.java

--- src/main/java/com/daily/domain/Member.java
+++ src/main/java/com/daily/domain/Member.java
@@ -6,26 +6,35 @@ public class Member implements Serializable {
   private static final long serialVersionUID = 1L;
   
   private int number;
+  private int type;
   private String id;
   private String password;
   private String name;
+  private String email;
+  private String tel;
   
   @Override
   public String toString() {
-    return "Member [number=" + number + ", id=" + id + ", password=" + password + ", name=" + name
-        + "]";
+    return "Member [number=" + number + ", type=" + type + ", id=" + id + ", password=" + password
+        + ", name=" + name + ", email=" + email + ", tel=" + tel + "]";
   }
+  
 
   @Override
   public int hashCode() {
     final int prime = 31;
     int result = 1;
+    result = prime * result + ((email == null) ? 0 : email.hashCode());
     result = prime * result + ((id == null) ? 0 : id.hashCode());
     result = prime * result + ((name == null) ? 0 : name.hashCode());
     result = prime * result + number;
     result = prime * result + ((password == null) ? 0 : password.hashCode());
+    result = prime * result + ((tel == null) ? 0 : tel.hashCode());
+    result = prime * result + type;
     return result;
   }
+
+
 
   @Override
   public boolean equals(Object obj) {
@@ -36,6 +45,11 @@ public class Member implements Serializable {
     if (getClass() != obj.getClass())
       return false;
     Member other = (Member) obj;
+    if (email == null) {
+      if (other.email != null)
+        return false;
+    } else if (!email.equals(other.email))
+      return false;
     if (id == null) {
       if (other.id != null)
         return false;
@@ -53,8 +67,50 @@ public class Member implements Serializable {
         return false;
     } else if (!password.equals(other.password))
       return false;
+    if (tel == null) {
+      if (other.tel != null)
+        return false;
+    } else if (!tel.equals(other.tel))
+      return false;
+    if (type != other.type)
+      return false;
     return true;
   }
+
+
+
+  public int getType() {
+    return type;
+  }
+
+
+  public Member setType(int type) {
+    this.type = type;
+    return this;
+  }
+
+
+  public String getEmail() {
+    return email;
+  }
+
+
+  public Member setEmail(String email) {
+    this.email = email;
+    return this;
+  }
+
+
+  public String getTel() {
+    return tel;
+  }
+
+
+  public Member setTel(String tel) {
+    this.tel = tel;
+    return this;
+  }
+
 
   public int getNumber() {
     return number;
