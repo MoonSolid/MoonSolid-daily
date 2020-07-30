efb5332ad7725c5bed93a21b074fd001e32c02ec
MoonSolid <rnreo20@gmail.com>
2020-07-30 20:46:40 +0900

    [add] MemberService 추가
    MemberService 인터페이스 추가
    add , findAll 메서드 추가
    
    - 추가
     - src/main/java/com/daily/service/MemberService.java

---
+++ src/main/java/com/daily/service/MemberService.java
@@ -0,0 +1,12 @@
+package com.daily.service;
+
+import java.util.List;
+import com.daily.domain.GeneralMember;
+import com.daily.domain.Member;
+
+public interface MemberService {
+
+  int add(Member member, GeneralMember generalMember) throws Exception;
+  
+  List<GeneralMember> findAll() throws Exception;
+}
