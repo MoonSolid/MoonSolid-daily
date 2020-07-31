823fe7767bd0508de93daa423b521de0cccd80cc
MoonSolid <rnreo20@gmail.com>
2020-08-01 02:09:08 +0900

    [mod] 일반회원 로그인 메서드 추가
    findByEmailAndPassword 메서드 추가
    
    - 수정
     - src/main/java/com/daily/dao/GeneralMemberDao.java

--- src/main/java/com/daily/dao/GeneralMemberDao.java
+++ src/main/java/com/daily/dao/GeneralMemberDao.java
@@ -1,12 +1,17 @@
 package com.daily.dao;
 
 import java.util.List;
+import java.util.Map;
 import com.daily.domain.GeneralMember;
 
 public interface GeneralMemberDao {
 
+  //일반회원가입
   int insert(GeneralMember generalMember) throws Exception;
   
   List<GeneralMember> findAll() throws Exception;
+  
+  //일반회원 로그인
+  GeneralMember findByEmailAndPassword(Map<String, Object> params) throws Exception;
   
 }
