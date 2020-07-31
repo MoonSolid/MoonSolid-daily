cd9cfca94e449c80b8eb160b2176382f8549581e
MoonSolid <rnreo20@gmail.com>
2020-08-01 01:57:59 +0900

    [mod] 로그인 기능을 위한 메서드 및 쿼리 추가
    findByEmailAndPassword 메서드 추가
    
    - 수정
     - src/main/java/com/daily/dao/MemberDao.java
    
    findByEmailAndPassword 쿼리 추가
    
    - 수정
     - src/main/resources/com/daily/mapper/MemberMapper.xml

--- src/main/java/com/daily/dao/MemberDao.java
+++ src/main/java/com/daily/dao/MemberDao.java
@@ -1,9 +1,14 @@
 package com.daily.dao;
 
+import java.util.Map;
 import com.daily.domain.Member;
 
 public interface MemberDao {
-
+  
+  //회원가입
   int insert(Member member) throws Exception;
+  
+  //로그인
+  Member findByEmailAndPassword(Map<String, Object> params) throws Exception;
   
 }
