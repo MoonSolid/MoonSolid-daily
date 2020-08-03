e74d825befefb3b25679e3883c0df1274a9881a0
MoonSolid <rnreo20@gmail.com>
2020-08-03 18:42:25 +0900

    [add] MainController 추가
    메인페이지를 위한 컨트롤러 추가
    
    - 추가
     - src/main/java/com/daily/web/MainController.java

---
+++ src/main/java/com/daily/web/MainController.java
@@ -0,0 +1,18 @@
+package com.daily.web;
+
+import javax.servlet.ServletContext;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestMapping;
+
+@Controller
+public class MainController {
+  
+  @Autowired
+  ServletContext servletContext;
+
+  @RequestMapping("/main")
+  public void main() throws Exception {
+    
+  }
+}
