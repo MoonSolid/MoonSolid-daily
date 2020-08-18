8773844d6d4f92ba3f8dcf1b0865ee9d504fa85f
MoonSolid <rnreo20@gmail.com>
2020-08-19 05:20:39 +0900

    [mod] NoticeDao 수정
    공지사항 조회 메서드 추가
    
    - 수정
     - src/main/java/com/daily/dao/NoticeDao.java

--- src/main/java/com/daily/dao/NoticeDao.java
+++ src/main/java/com/daily/dao/NoticeDao.java
@@ -1,9 +1,12 @@
 package com.daily.dao;
 
+import java.util.List;
 import com.daily.domain.Notice;
 
 public interface NoticeDao {
   
   int insert(Notice notice) throws Exception;
+  
+  List<Notice> findAll() throws Exception;
 
 }
