37431dab603c678240c3c1a3a510b7bf22210536
MoonSolid <rnreo20@gmail.com>
2020-08-18 20:54:05 +0900

    [add] NoticeCategory 추가
    공지사항 카테고리 도메인 추가
    
    - 추가
     - src/main/java/com/daily/domain/NoticeCategory.java

---
+++ src/main/java/com/daily/domain/NoticeCategory.java
@@ -0,0 +1,63 @@
+package com.daily.domain;
+
+import java.io.Serializable;
+
+public class NoticeCategory implements Serializable {
+  private static final long serialVersionUID = 1L;
+  
+  private int categoryNumber;
+  private String name;
+  
+  @Override
+  public String toString() {
+    return "NoticeCategory [categoryNumber=" + categoryNumber + ", name=" + name + "]";
+  }
+
+  @Override
+  public int hashCode() {
+    final int prime = 31;
+    int result = 1;
+    result = prime * result + categoryNumber;
+    result = prime * result + ((name == null) ? 0 : name.hashCode());
+    return result;
+  }
+
+  @Override
+  public boolean equals(Object obj) {
+    if (this == obj)
+      return true;
+    if (obj == null)
+      return false;
+    if (getClass() != obj.getClass())
+      return false;
+    NoticeCategory other = (NoticeCategory) obj;
+    if (categoryNumber != other.categoryNumber)
+      return false;
+    if (name == null) {
+      if (other.name != null)
+        return false;
+    } else if (!name.equals(other.name))
+      return false;
+    return true;
+  }
+
+  public int getCategoryNumber() {
+    return categoryNumber;
+  }
+
+  public NoticeCategory setCategoryNumber(int categoryNumber) {
+    this.categoryNumber = categoryNumber;
+    return this;
+  }
+
+  public String getName() {
+    return name;
+  }
+
+  public NoticeCategory setName(String name) {
+    this.name = name;
+    return this;
+  }
+  
+  
+}
